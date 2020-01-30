578cc67e13230942b1fbf98208eb7322aa30844b
Basit Hamid <basithamid@gmail.com>
2020-01-30 18:03:00 +0000

    Create Bike Job (#3)

--- DataRetrieval/cron.py
+++ DataRetrieval/cron.py
@@ -1,7 +1,12 @@
 import kronos
 import random
 from DataRetrieval.pollution import pollution_job
+from DataRetrieval.bike import bikes_job
 
 @kronos.register('* * * * *')
 def execute_pollution_job():
     pollution_job.PollutionJob().exec()
+
+@kronos.register('* * * * *')
+def execute_bikes_job():
+    bikes_job.BikeJob().exec()
