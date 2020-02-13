c9e1c8efab71f607aeea2fe7914524164ec480c7
rgog <rajat.gogna30@gmail.com>
2020-02-13 16:42:23 +0000

    Add Color field to Traffic Data

--- DataRetrieval/traffic/traffic_util.py
+++ DataRetrieval/traffic/traffic_util.py
@@ -66,9 +66,20 @@ class TrafficUtil():
         elif('httpStatusCode' in response and response['httpStatusCode'] == status.HTTP_400_BAD_REQUEST):
             return None
         else:
+            curSpeed = response["flowSegmentData"]["currentSpeed"]
+            freeSpeed = response["flowSegmentData"]["freeFlowSpeed"]
+
+            if curSpeed/freeSpeed <= 0.6:
+                if curSpeed / freeSpeed <= 0.2:
+                    color = "#ff0000x"
+                else:
+                    color = "#FFBF00"
+            else:
+                color = "#00b300"
             data = {
                 "lat" : lat,
                 "long" : lng,
+                "color" : color,
                 "currentSpeed" : response["flowSegmentData"]["currentSpeed"],
                 "freeFlowSpeed" : response["flowSegmentData"]["freeFlowSpeed"],
                 "currentTravelTime" : response["flowSegmentData"]["currentTravelTime"],
