4f13956b508ba8c9c11b317426a4c07aba45f877
Sumit Mukhija <sumitmukhija@hotmail.com>
2020-02-25 12:55:51 +0000

    Removed print statements

--- DataInteractions/traffic/traffic_mongo_test.py
+++ DataInteractions/traffic/traffic_mongo_test.py
@@ -25,11 +25,9 @@ class TrafficMongoTest(SimpleTestCase):
     def test_insertion_api_analysis(self):
         data = open('./test_data_traffic_analysis.json', 'r')
         request_json = data.read()
-        print(request_json)
         valid_token = TestUtils().get_valid_auth()
         headers = {"Authorization": valid_token}
         response = requests.post(url=Environ().get_base_traffic_url(), data={"data": request_json}, headers=headers)
-        print(response.text())
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test__analysis_retrieval_by_lat_long_function(self):
