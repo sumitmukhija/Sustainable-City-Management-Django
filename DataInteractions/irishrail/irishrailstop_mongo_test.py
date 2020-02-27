959608d48a7f18b7ddc87a7bbdc1f3ebbd5fe181
Basit Hamid <basithamid@gmail.com>
2020-02-27 15:53:25 +0000

    Fix APIs authentication

--- DataInteractions/irishrail/irishrailstop_mongo_test.py
+++ DataInteractions/irishrail/irishrailstop_mongo_test.py
@@ -1,17 +1,19 @@
 from django.test import SimpleTestCase
 from rest_framework import status
 from SCMBackend.env import Environ
+from DataInteractions.test_utils import TestUtils
 import requests
 
+valid_token = TestUtils().get_valid_auth()
+headers = {"Authorization": valid_token}
 
 class IrishRailStopMongoTest(SimpleTestCase):
     def test_insertion_api(self):
         data = open('./test_data_irishrailstop.json', 'r')
         request_json = data.read()
-        print(request_json)
-        response = requests.post(url=Environ().get_base_irish_rail_stop_url(), data={"data": request_json})
+        response = requests.post(url=Environ().get_base_irish_rail_stop_url(), data={"data": request_json}, headers=headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_retrieval_by_lat_long_function(self):
-        response = requests.get(url=Environ().get_base_irish_rail_stop_url())
+        response = requests.get(url=Environ().get_base_irish_rail_stop_url(), headers=headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.status_code)
