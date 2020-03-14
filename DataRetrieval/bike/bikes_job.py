379c67e1f3612dd33c12a3dcc88d6a28c2a21cf0
rgog <rajat.gogna30@gmail.com>
2020-03-14 18:26:45 +0000

    Fixed Baqi index bug, refactored POST API calls

--- DataRetrieval/bike/bikes_job.py
+++ DataRetrieval/bike/bikes_job.py
@@ -11,7 +11,7 @@ class BikeJob(cron_job.CronJob):
 
     def run_job(self):
         headers = {"Authorization": TestUtils().get_valid_auth()}
-        url = Environ().get_base_bus_stop_url()
+        url = Environ().get_base_bike_url()
         bikes_data = bike_util.BikeUtil.get_dublin_bikes_data()
         response = bike_util.BikeUtil.format_dublin_bikes_data(bikes_data)
         response = requests.post(url, json={"data": json.dumps(response)}, headers=headers)
--- DataRetrieval/busstops/busstop_job.py
+++ DataRetrieval/busstops/busstop_job.py
@@ -4,24 +4,15 @@ from SCMBackend.env import Environ
 from DataInteractions.test_utils import TestUtils
 import requests
 import json
-import datetime
 
 class BusJob(cron_job.CronJob):
 
     def run_job(self):
         headers = {"Authorization": TestUtils().get_valid_auth()}
         url = Environ().get_base_bus_stop_url()
-        timestamp = str(datetime.datetime.now())
         stops = busstop_util.BusStopUtil().get_bus_stop_coordinates()
         if (stops is not None):
-            for s in range(stops.shape[0]):
-                data = {
-                    "StopID": stops.iloc[s, 0],
-                    "StopName": stops.iloc[s, 1],
-                    "lat": stops.iloc[s, 2],
-                    "long": stops.iloc[s, 3],
-                    "timestamp": timestamp
-                }
+            data = busstop_util.BusStopUtil.format_data(stops)
             response = requests.post(url, json={"data": json.dumps(data)}, headers=headers)
         else:
-            print("Bus Stop data unavailable")
+            pass
