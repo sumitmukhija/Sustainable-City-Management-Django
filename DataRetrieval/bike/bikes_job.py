bd03add829ca4f75c2c132d0cc984ae8c4a144d1
rgog <rajat.gogna30@gmail.com>
2020-03-14 16:53:42 +0000

    Add Auth to Jobs, add timestamp field to data

--- DataRetrieval/bike/bikes_job.py
+++ DataRetrieval/bike/bikes_job.py
@@ -1,16 +1,18 @@
 from DataRetrieval import cron_job
 from DataRetrieval.bike import bike_util
+from SCMBackend.env import Environ
+from DataInteractions.test_utils import TestUtils
 import datetime
 import requests
 import os
 import json
-from SCMBackend.env import Environ
 
 class BikeJob(cron_job.CronJob):
 
     def run_job(self):
+        headers = {"Authorization": TestUtils().get_valid_auth()}
         url = Environ().get_base_bus_stop_url()
         bikes_data = bike_util.BikeUtil.get_dublin_bikes_data()
         response = bike_util.BikeUtil.format_dublin_bikes_data(bikes_data)
-        response = requests.post(url, json={"data": json.dumps(response)})
+        response = requests.post(url, json={"data": json.dumps(response)}, headers=headers)
 
--- DataRetrieval/luasstops/luasstop_job.py
+++ DataRetrieval/luasstops/luasstop_job.py
@@ -1,13 +1,17 @@
 from DataRetrieval import cron_job
 from DataRetrieval.luasstops import luasstop_util
+from SCMBackend.env import Environ
+from DataInteractions.test_utils import TestUtils
 import requests
 import json
-from SCMBackend.env import Environ
+import datetime
 
 
 class LuasJob(cron_job.CronJob):
 
     def run_job(self):
+        headers = {"Authorization": TestUtils().get_valid_auth()}
+        timestamp = str(datetime.datetime.now())
         url = Environ().get_base_luas_stop_url()
         stops = luasstop_util.LuasStopUtil().get_luas_stop_coordinates()
         if (stops is not None):
@@ -16,8 +20,9 @@ class LuasJob(cron_job.CronJob):
                     "StopID": stops.iloc[s, 0],
                     "StopName": stops.iloc[s, 1],
                     "lat": stops.iloc[s, 2],
-                    "long": stops.iloc[s, 3]
+                    "long": stops.iloc[s, 3],
+                    "timestamp": timestamp
                 }
-                response = requests.post(url, json={"data": json.dumps(data)})
+                response = requests.post(url, json={"data": json.dumps(data)}, headers=headers)
         else:
             print("Luas Stop data unavailable")
