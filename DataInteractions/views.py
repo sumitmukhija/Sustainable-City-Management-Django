105366c79fb87c6e7f4a50549cab4f856186c3a7
ag6102 <aashima.goel91@gmail.com>
2020-04-03 18:30:49 +0100

    pusher changes

--- DataInteractions/views.py
+++ DataInteractions/views.py
@@ -10,15 +10,18 @@ from DataInteractions.traffic.traffic_data_interactions import TrafficDataIntera
 from DataInteractions.busstops.busstop_data_interactions import BusStopDataInteractions
 from DataInteractions.luasstops.luasstop_data_interactions import LuasStopDataInteractions
 from DataInteractions.irishrail.irishrailstop_data_interactions import IrishRailStopDataInteractions
+from DataInteractions.alerts.alerts_data_interactions import AlertsDataInteractions
 import json
 from mongo_auth.permissions import AuthenticatedOnly
 from mongo_auth.utils import login_status
+from django.core.cache import cache
+from SCMBackend.notifications import Notifier
 
 
 class PollDetails(APIView):
-    
+
     permission_classes = [AuthenticatedOnly]
-    
+
     def post(self, request, format=None):
         data = request.data['data']
         if data is None:
@@ -38,7 +41,7 @@ class PollDetails(APIView):
 
 class DublinBikeDetails(APIView):
 
-    permission_classes = [AuthenticatedOnly,BikeAuth]
+    permission_classes = [AuthenticatedOnly, BikeAuth]
 
     def post(self, request, format=None):
         data = request.data['data']
@@ -63,8 +66,8 @@ class CheckAuthentication(APIView):
         try:
             flag, user_obj = login_status(request)
             response = {
-                "authorized":flag,
-                "user":user_obj
+                "authorized": flag,
+                "user": user_obj
             }
             return Response(response, status=status.HTTP_200_OK)
         except Exception as e:
@@ -74,8 +77,9 @@ class CheckAuthentication(APIView):
             }
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
+
 class TrafficAnalysisDetails(APIView):
-    
+
     permission_classes = [AuthenticatedOnly]
 
     def post(self, request, format=None):
@@ -93,7 +97,6 @@ class TrafficAnalysisDetails(APIView):
         response = TrafficDataInteractions().get_latest_analysis_data_by_lat_long()
         responseStatus = status.HTTP_200_OK if response is not None else status.HTTP_404_NOT_FOUND
         return Response(response, status=responseStatus)
-
 
 
 class TrafficDetails(APIView):
@@ -158,8 +161,9 @@ class LuasStopDetails(APIView):
         responseStatus = status.HTTP_200_OK if response is not None else status.HTTP_404_NOT_FOUND
         return Response(response, status=responseStatus)
 
+
 class IrishRailStopDetails(APIView):
-    
+
     permission_classes = [AuthenticatedOnly, DartAuth]
 
     def post(self, request, format=None):
@@ -175,5 +179,45 @@ class IrishRailStopDetails(APIView):
 
     def get(self, request):
         response = IrishRailStopDataInteractions().get_latest_by_lat_long()
+        responseStatus = status.HTTP_200_OK if response is not None else status.HTTP_404_NOT_FOUND
+        return Response(response, status=responseStatus)
+
+
+class FlagDetails(APIView):
+
+    permission_classes = [AuthenticatedOnly]
+
+    def post(self, request, format=None):
+        print(request.data)
+        if request.data is None:
+            return Response("No Data", status=status.HTTP_400_BAD_REQUEST)
+        try:
+            x = cache.set("isAlertPresent",
+                          request.data["isAlertPresent"], timeout=None)
+        except Exception as e:
+            return Response('Exception', status=status.HTTP_400_BAD_REQUEST)
+        return Response('Success', status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        return Response({'isAlertPresent': cache.get("isAlertPresent")}, status=status.HTTP_200_OK)
+
+
+class AlertList(APIView):
+    def post(self, request, format=None):
+        data = request.data
+        if request is None:
+            return Response("No Data", status=status.HTTP_400_BAD_REQUEST)
+        # data = json.loads(data)
+        try:
+            x = AlertsDataInteractions().update_alert(data)
+            notifier = Notifier()
+            notifier.dispatch_notification(
+                "test notification 1", "to-service-provider")
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        return Response(str(x), status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        response = AlertsDataInteractions().get_alerts()
         responseStatus = status.HTTP_200_OK if response is not None else status.HTTP_404_NOT_FOUND
         return Response(response, status=responseStatus)
